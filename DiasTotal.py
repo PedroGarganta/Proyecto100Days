4c9017ee26c957e59a9e8eb6b23b312fd162574e
Pedro Garganta <pedrogarganta00@gmail.com>
2024-05-28 10:43:21 -0300

    .

---
+++ DiasTotal.py
@@ -0,0 +1,16 @@
+###################### DIA 6 ##################################
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        tot= len(nums)
+        for i in range(k):
+                ultimo = nums[tot];
+                for j in range(0,tot):
+                         nums[tot-j]= nums[tot-j-1]
+                nums[0]= ultimo;
+
